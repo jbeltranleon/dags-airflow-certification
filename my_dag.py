13deaaa5424586b6c4bf8bc1662831b6820041a7
jbeltranleon <jbeltranleon@gmail.com>
2021-12-22 15:14:26 -0500

    use json variable

--- my_dag.py
+++ my_dag.py
@@ -4,15 +4,28 @@ from datetime import datetime, timedelta
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 
+# Do not get variables outside a function, this creates a lot of useless connections (affect db)
+# variable = Variable.get('my_dag_variable')
+
 
 def _extract():
-    variable = Variable.get('my_dag_variable')
+    # variable = Variable.get('my_dag_variable')
+
+    # Use dicts instead of generating individual variables
+    settings = Variable.get('my_dag_settings', deserialize_json=True)
+
+    email = settings.get('email')
+    hobbies = settings.get('hobbies')
+
     secret = Variable.get('my_dag_secret')
 
-    print(variable, secret)
+    print(email, secret, hobbies[0])
 
 
 with DAG(dag_id="my_dag", description="DAG in charge of ... *This allow Markdown*", start_date=datetime(2021, 11, 4),
          schedule_interval="@daily", dagrun_timeout=timedelta(minutes=10), tags=["data_science"], catchup=False) as dag:
+
+    # Do not get variables outside a function, this creates a lot of useless connections (affect db)
+    # variable = Variable.get('my_dag_variable')
 
     extract = PythonOperator(task_id="extract", python_callable=_extract)
