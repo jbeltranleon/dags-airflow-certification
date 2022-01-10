80bb104ab3c2adde34d759ea1e80f5302bf77f53
jbeltranleon <jbeltranleon@gmail.com>
2022-01-09 22:02:57 -0500

    use get_current_context to share xcoms between dag and subdag

--- sub_dag/child_dag.py
+++ sub_dag/child_dag.py
@@ -1,18 +1,14 @@
 import base64
-import requests
 
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import get_current_context
 
 
-@task.python(multiple_outputs=True)
-def get_response():
-    return requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
-
-
-def _encode_title(ti):
-    title = ti.xcom_pull(key="title", task_ids="get_response")
+@task.python
+def encode_title():
+    ti = get_current_context()['ti']
+    title = ti.xcom_pull(key="title", task_ids="get_response", dag_id="main")
 
     title_bytes = title.encode('ascii')
     base64_bytes = base64.b64encode(title_bytes)
@@ -20,14 +16,18 @@ def _encode_title(ti):
     return base64_title
 
 
-def _set_username(ti):
-    uid = ti.xcom_pull(key="userId", task_ids="get_response")
+@task.python
+def set_username():
+    ti = get_current_context()['ti']
+    uid = ti.xcom_pull(key="userId", task_ids="get_response", dag_id="main")
     usernames = {1: 'Bob'}
     return usernames.get(uid)
 
 
-def _build_file(ti):
-    completed = ti.xcom_pull(key="completed", task_ids="get_response")
+@task.python
+def build_file():
+    ti = get_current_context()['ti']
+    completed = ti.xcom_pull(key="completed", task_ids="get_response", dag_id="main")
     encoded_title = ti.xcom_pull(task_ids="encode_title")
     username = ti.xcom_pull(task_ids="set_username")
 
@@ -37,10 +37,7 @@ def _build_file(ti):
 
 def subdag_factory(parent_dag_id, subdag_dag_id, default_args):
     with DAG(dag_id=f"{parent_dag_id}.{subdag_dag_id}", default_args=default_args) as dag:
-        encode_title = PythonOperator(task_id="encode_title", python_callable=_encode_title)
-        set_username = PythonOperator(task_id="set_username", python_callable=_set_username)
-        build_file = PythonOperator(task_id="build_file", python_callable=_build_file)
 
-        get_response() >> [encode_title, set_username] >> build_file
+        [encode_title(), set_username()] >> build_file()
 
     return dag
--- sub_dag/main_dag.py
+++ sub_dag/main_dag.py
@@ -1,3 +1,5 @@
+import requests
+
 from datetime import timedelta, datetime
 
 from airflow import DAG
@@ -9,16 +11,16 @@ from dags.sub_dag.child_dag import subdag_factory
 default_args = {"start_date": datetime(2022, 1, 7)}
 
 
+@task.python(multiple_outputs=True)
+def get_response():
+    return requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+
+
 @task.python
 def read_file():
     with open(file='file.csv', mode='r') as f:
         for line in f.readlines():
             print(line)
-
-
-@task.python
-def start():
-    print("start")
 
 
 with DAG(dag_id="main", description="Main Dag - SubDag example", dagrun_timeout=timedelta(minutes=10),
@@ -29,4 +31,4 @@ with DAG(dag_id="main", description="Main Dag - SubDag example", dagrun_timeout=
         subdag=subdag_factory(parent_dag_id="main", subdag_dag_id="process", default_args=default_args)
     )
 
-    start() >> transform >> read_file()
+    get_response() >> transform >> read_file()
